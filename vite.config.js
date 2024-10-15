9a6615ddcb057fda4e7700bcc7bf986ca1db7485
Martin <martin.kolberg@outlook.com>
2024-10-15 17:10:10 +0200

    joda

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://mrmarre.github.io/IntervalTimer-react-docker/',
+  base: 'IntervalTimer-react-docker/',
   server: {
     watch: {
       usePolling: true,
